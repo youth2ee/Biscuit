b64d3da81df13fe75662483d671b3be12e23c99b
youth2ee <sprirealee@gmail.com>
2019-12-25 17:22:11 +0900

    1

---
+++ src/test/java/com/biscuit/b1/TestAbstractCase.java
@@ -0,0 +1,13 @@
+package com.biscuit.b1;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/Web-INF/spring/**/*-context.xml"})
+public class TestAbstractCase {
+
+}
+
+
